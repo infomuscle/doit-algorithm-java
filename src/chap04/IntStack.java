f75c50f13f82b991d32c0fd1f8b05c92588db39d
infomuscle <infomuscle10@gmail.com>
2020-04-08 21:36:47 +0900

    04-1 IntStack

---
+++ src/chap04/IntStack.java
@@ -0,0 +1,25 @@
+package chap04;
+
+public class IntStack {
+    private int max;
+    private int ptr;
+    private int[] stk;
+
+    public class EmptyIntStackException extends RuntimeException {
+        public EmptyIntStackException() {}
+    }
+
+    public class OverflowIntStackException extends RuntimeException {
+        public OverflowIntStackException() {}
+    }
+
+    public IntStack(int capacity) {
+        ptr = 0;
+        max = capacity;
+        try {
+            stk = new int[max];
+        } catch (OutOfMemoryError e) {
+            max = 0;
+        }
+    }
+}
