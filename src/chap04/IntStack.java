466d2164da5f7f27de855c5c89005f60d2d4881f
infomuscle <infomuscle10@gmail.com>
2020-04-08 21:40:26 +0900

    04-1 IntStack

--- src/chap04/IntStack.java
+++ src/chap04/IntStack.java
@@ -22,4 +22,11 @@ public class IntStack {
             max = 0;
         }
     }
+
+    public int push(int x) throws OverflowIntStackException {
+        if (ptr >= max) {
+            throw new OverflowIntStackException();
+        }
+        return stk[ptr++] = x;
+    }
 }
