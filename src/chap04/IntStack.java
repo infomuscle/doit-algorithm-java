eb130d74fe56e53b75bd82004bf9267a2b6a3875
infomuscle <infomuscle10@gmail.com>
2020-04-08 22:01:39 +0900

    04-1 IntStack

--- src/chap04/IntStack.java
+++ src/chap04/IntStack.java
@@ -6,11 +6,13 @@ public class IntStack {
     private int[] stk;
 
     public class EmptyIntStackException extends RuntimeException {
-        public EmptyIntStackException() {}
+        public EmptyIntStackException() {
+        }
     }
 
     public class OverflowIntStackException extends RuntimeException {
-        public OverflowIntStackException() {}
+        public OverflowIntStackException() {
+        }
     }
 
     public IntStack(int capacity) {
@@ -42,5 +44,45 @@ public class IntStack {
             throw new EmptyIntStackException();
         }
         return stk[ptr - 1];
+    }
+
+    public int indexOf(int x) {
+        for (int i = ptr - 1; i >= 0; i--) {
+            if (stk[i] == x) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void clear() {
+        ptr = 0;
+    }
+
+    public int capacity() {
+        return max;
+    }
+
+    public int size() {
+        return ptr;
+    }
+
+    public boolean isEmpty() {
+        return ptr <= 0;
+    }
+
+    public boolean isFull() {
+        return ptr >= max;
+    }
+
+    public void dump() {
+        if (ptr <= 0) {
+            System.out.println("스택이 비어있습니다.");
+        } else {
+            for (int i = 0; i < ptr; i++) {
+                System.out.print(stk[i] + " ");
+            }
+            System.out.println();
+        }
     }
 }
